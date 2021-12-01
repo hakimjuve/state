c5e99a46b4cc0768773b8be9f2ca5411f40ed443
hakimjuve <hakem.hakem95@gmail.com>
2021-12-01 12:27:40 +0100

    Adding Chekpoint

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Button , Card } from 'react-bootstrap';
+
+
+export default class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            counter: 0
+        }
+    }
+
+    increse = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+    decrese=()=>{
+      if (this.state.counter >0){ 
+      this.setState({
+        counter: this.state.counter - 1
+        });
+    }
+    }
+
+    reset = () => {
+        this.setState({
+            counter: 0
+        })
+
+    }
+
+
+
+
+    render() {
+        return (
+            <div className="App">
+<div style={{ width: '24.6rem' }}>
+<Card>
+  <Card.Img variant="top"  src="/tshrt.jpg" alt="image" />
+  <Card.Body>
+    <Card.Title style={{textAlign:"center"}}>Juvents Kit 2021/22</Card.Title>
+    <p style={{textAlign:"center"}} >Price: 120$ <br /> Size: S L XL 2XL 3XL</p>
+    
+    <p style={{border:"solid 2px #e6e6e6", fontSize:"30px", textAlign:"center",borderRadius:"10px", width:"75px", marginLeft:"150px"}}> {this.state.counter}</p>
+
+    <Button variant="primary" onClick={this.increse}>+</Button>
+    <Button variant="primary" onClick={this.decrese} >-</Button>
+    <Button variant="warning" onClick={this.reset} >Reset</Button>
+  </Card.Body>
+</Card>
+
+</div>
+            </div>
+        )
+    }
+}
